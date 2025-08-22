1f6f7da14e6aac8403ec020148923a0438c7a8f2
Ivan <225489253+XivxnX@users.noreply.github.com>
2025-08-21 18:09:15 -0600

    Diseño inicial de la app. Se agrega un botón para construir un histograma

---
+++ app.py
@@ -0,0 +1,21 @@
+# importar las librerías necesarias
+import pandas as pd
+import plotly_express as px
+import streamlit as st
+
+# cargar el dataset
+car_data = pd.read_csv('vehicles_us.csv')
+
+# contenido de la aplicación Streamlit
+st.header('Análisis Exploratorio de vehículos')  # agregar encabezado
+hist_button = st.button('Construir Histograma') # botón para construir el histograma
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Construyendo un histograma de los odómetros de los vehículos...')
+    
+    # crear un histograma
+    fig = px.histogram(car_data, x='odometer')
+
+    # mostrar gráfico plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
