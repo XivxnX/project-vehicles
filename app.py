e770fac3d21588c3e486885dc7c15162fcb682a7
Ivan <225489253+XivxnX@users.noreply.github.com>
2025-08-21 18:11:22 -0600

    Upgrade app. Se agrega un checkbox para construir un gráfico de dispersión

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ car_data = pd.read_csv('vehicles_us.csv')
 # contenido de la aplicación Streamlit
 st.header('Análisis Exploratorio de vehículos')  # agregar encabezado
 hist_button = st.button('Construir Histograma') # botón para construir el histograma
+scatter_checkbox = st.checkbox('Construir Gráfico de Dispersión') # checkbox para construir el gráfico de dispersión
 
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
@@ -17,5 +18,15 @@ if hist_button:  # al hacer clic en el botón
     # crear un histograma
     fig = px.histogram(car_data, x='odometer')
 
+    # mostrar gráfico plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if scatter_checkbox:  # al marcar la casilla
+    # escribir un mensaje
+    st.write('Construyendo un gráfico de dispersión de los odómetros de los vehículos...')
+    
+    # crear un gráfico de dispersión
+    fig = px.scatter(car_data, x='odometer')
+    
     # mostrar gráfico plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
